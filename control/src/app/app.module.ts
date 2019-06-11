2b9b85d55cc6561153125bdbb118994e868d1ab0
GerardBR3270 <47857288+GerardBR3270@users.noreply.github.com>
2019-06-11 08:07:53 -0500

    more components
    a lot of more fuckin components

--- control/src/app/app.module.ts
+++ control/src/app/app.module.ts
@@ -6,13 +6,19 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { ComunicationComponent } from './components/comunication/comunication.component';
+import { AddUserComponent } from './component/add-user/add-user.component';
+import { AddPaymentComponent } from './component/add-payment/add-payment.component';
+import { AddChargeComponent } from './component/add-charge/add-charge.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     NavbarComponent,
-    ComunicationComponent
+    ComunicationComponent,
+    AddUserComponent,
+    AddPaymentComponent,
+    AddChargeComponent
   ],
   imports: [
     BrowserModule,
---
+++ control/src/app/component/add-charge/add-charge.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-charge',
+  templateUrl: './add-charge.component.html',
+  styleUrls: ['./add-charge.component.sass']
+})
+export class AddChargeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
