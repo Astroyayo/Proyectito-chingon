fb7a3e8e3e8df27ba38649efa7fb1029ccd30163
GerardBR3270 <47857288+GerardBR3270@users.noreply.github.com>
2019-06-11 00:41:33 -0500

    Components
    creacion de componentes

--- control/src/app/app.module.ts
+++ control/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ComunicationComponent } from './components/comunication/comunication.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NavbarComponent,
+    ComunicationComponent
   ],
   imports: [
     BrowserModule,
